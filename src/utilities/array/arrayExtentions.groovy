c64fdba85f987a5936869261ece9672a8eb98a02
Felipe Gorigoitia <felipe.gorigoitia@gmail.com>
2022-02-06 11:38:06 -0300

    fix name on utility

--- src/utilities/array/arrayExtentions.groovy
+++ src/utilities/array/arrayExtentions.groovy
@@ -2,6 +2,9 @@ package utilities.array
 ​
 def searchKeyInArray(String keyWordsAsString, String splitIdentifier, Map arrayMapToCompare){
     def _array = []
+    if (!keyWordsAsString) {
+      return []
+    }
     keyWordsAsString.split("${splitIdentifier}").each{
         def _key = it?.trim()
         if(!_key.equals("") && ( arrayMapToCompare.containsKey(it) )){
